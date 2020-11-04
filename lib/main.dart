8ddcd34527d9368e403fbdc9fd4256b62edb8d2e
Yevhen Ivanov <ivanovev77@gmail.com>
2020-11-04 11:01:17 +0200

    implementation of void randomGenerator function

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,3 @@
-import 'dart:math';
-
 import 'package:flutter/material.dart';
 import 'dart:math' as math;
 
@@ -13,37 +11,32 @@ class MyApp extends StatefulWidget {
 }
 
 class _MyAppState extends State<MyApp> {
+  Color colorOfScreen;
+  math.Random random = new math.Random();
+
+  void randomGenerator() {
+    setState(() {
+      colorOfScreen = Color.fromARGB(
+          255, random.nextInt(255), random.nextInt(255), random.nextInt(255));
+      print('color has changed!');
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
-    Color colorOfScreen;
-
-    void randomGenerator() {
-      Random random = new math.Random();
-      setState(() {
-        colorOfScreen = Color.fromARGB(
-            255, random.nextInt(255), random.nextInt(255), random.nextInt(255));
-      });
-      print('color has changed!');
-    }
-
     return GestureDetector(
       onTap: randomGenerator,
       child: MaterialApp(
         debugShowCheckedModeBanner: false,
         home: Scaffold(
           backgroundColor: colorOfScreen,
-          body: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              Center(
-                child: Container(
-                  child: Text(
-                    'Hey there!',
-                    style: TextStyle(fontSize: 30, color: Colors.black),
-                  ),
-                ),
-              )
-            ],
+          body: Center(
+            child: Container(
+              child: Text(
+                'Hey there!',
+                style: TextStyle(fontSize: 30, color: Colors.black),
+              ),
+            ),
           ),
         ),
       ),
