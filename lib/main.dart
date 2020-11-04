892f7c254d5e7570c04ef4308c65b7732cf74cb0
Yevhen Ivanov <ivanovev77@gmail.com>
2020-11-04 15:45:44 +0200

    implementation of  toogle_button

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_hsvcolor_picker/flutter_hsvcolor_picker.dart';
 import 'dart:math' as math;
 
 void main() {
@@ -14,27 +15,78 @@ class _MyAppState extends State<MyApp> {
   Color colorOfScreen;
   math.Random random = new math.Random();
 
-  void randomGenerator() {
-    setState(() {
-      colorOfScreen = Color.fromARGB(
-          255, random.nextInt(255), random.nextInt(255), random.nextInt(255));
-      print('color has changed!');
-    });
+  List<bool> _selections = List.generate(2, (_) => false);
+
+  void colorGenerator() {
+    if (_selections[0] == true) {
+      setState(() {
+        colorOfScreen = Color.fromARGB(
+            255, random.nextInt(255), random.nextInt(255), random.nextInt(255));
+        print('color has changed!');
+      });
+    } else {
+      // setState(() => currentColor = pickerColor);
+    }
   }
 
   @override
   Widget build(BuildContext context) {
     return GestureDetector(
-      onTap: randomGenerator,
+      onTap: colorGenerator,
       child: MaterialApp(
         debugShowCheckedModeBanner: false,
-        home: Scaffold(
-          backgroundColor: colorOfScreen,
-          body: Center(
-            child: Container(
-              child: Text(
-                'Hey there!',
-                style: TextStyle(fontSize: 30, color: Colors.black),
+        home: SafeArea(
+          child: Scaffold(
+            backgroundColor: colorOfScreen,
+            body: Center(
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.center,
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                children: [
+                  Container(
+                    margin: EdgeInsets.only(top: 30),
+                    child: ToggleButtons(
+                      color: Colors.black,
+                      selectedColor: Colors.amberAccent,
+                      disabledColor: Colors.green[100],
+                      fillColor: Colors.deepPurple,
+                      children: <Widget>[
+                        Padding(
+                          padding: const EdgeInsets.all(8.0),
+                          child: Text('Random Color'),
+                        ),
+                        Padding(
+                          padding: const EdgeInsets.all(8.0),
+                          child: Text('ColorPicker'),
+                        ),
+                      ],
+                      onPressed: (int index) {
+                        setState(() {
+                          for (int buttonIndex = 0;
+                              buttonIndex < _selections.length;
+                              buttonIndex++) {
+                            if (buttonIndex == index) {
+                              _selections[buttonIndex] = true;
+                            } else {
+                              _selections[buttonIndex] = false;
+                            }
+                          }
+                        });
+                      },
+                      isSelected: _selections,
+                    ),
+                  ),
+                  Container(
+                    child: Text(
+                      'Hey there!',
+                      style: TextStyle(fontSize: 30, color: Colors.black),
+                    ),
+                  ),
+                  Container(
+                    child:
+                        ColorPicker(color: Colors.blue, onChanged: (value) {}),
+                  ),
+                ],
               ),
             ),
           ),
