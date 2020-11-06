59292a0b0216098fbe5202cbf41eec53f1e58626
Yevhen Ivanov <ivanovev77@gmail.com>
2020-11-06 23:29:21 +0200

    addition of animated container

---
+++ lib/color_changer.dart
@@ -0,0 +1,110 @@
+import 'package:flutter/material.dart';
+import 'package:change_color_app/text_for_screen.dart';
+import 'dart:math' as math;
+import 'animated_figures.dart';
+
+enum _generationMode { random, manual }
+
+class ColorChanger extends StatefulWidget {
+  @override
+  _ColorChangerState createState() => _ColorChangerState();
+}
+
+class _ColorChangerState extends State<ColorChanger> {
+  List<Widget> scoreKeeper = [];
+
+  Color randomColorOfScreen;
+  math.Random random = new math.Random();
+
+  _generationMode _mode = _generationMode.random;
+
+
+
+  List<bool> _selections = List.generate(2, (_) => false);
+
+  void randomColorGenerator() {
+    if (_selections[0] == true) {
+      setState(() {
+        randomColorOfScreen = Color.fromARGB(
+            255, random.nextInt(255), random.nextInt(255), random.nextInt(255));
+        print('color has changed!');
+      });
+    } else {
+      setState(() {
+        randomColorOfScreen = null;
+      });
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return GestureDetector(
+      onTap: randomColorGenerator,
+      child: MaterialApp(
+        debugShowCheckedModeBanner: false,
+        home: Scaffold(
+          backgroundColor: randomColorOfScreen,
+          body: Column(
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            children: [
+              Expanded(
+                child: Center(
+                  child: _display(_mode),
+                ),
+              ),
+              Container(
+                margin: EdgeInsets.fromLTRB(0, 0, 15, 15),
+                child: Stack(alignment: Alignment.bottomRight, children: [
+                  ToggleButtons(
+                    color: Colors.black,
+                    borderColor: Colors.red[700],
+                    selectedColor: Colors.amberAccent,
+                    disabledColor: Colors.grey,
+                    fillColor: Colors.orange,
+                    children: <Widget>[
+                      Padding(
+                        padding: const EdgeInsets.all(8.0),
+                        child: Text('Random Color'),
+                      ),
+                      Padding(
+                        padding: const EdgeInsets.all(8.0),
+                        child: Text('ColorPicker'),
+                      ),
+                    ],
+                    onPressed: (int index) {
+                      setState(() {
+                        for (int buttonIndex = 0;
+                        buttonIndex < _selections.length;
+                        buttonIndex++) {
+                          if (buttonIndex == index) {
+                            _selections[buttonIndex] = true;
+                            _mode = _generationMode.manual;
+                          } else {
+                            _selections[buttonIndex] = false;
+                            _mode = _generationMode.random;
+                          }
+                        }
+                        print('$_mode');
+                      });
+                    },
+                    isSelected: _selections,
+                  ),
+                ]),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+Widget _display(modeOfState) {
+  if (modeOfState == _generationMode.random) {
+    return TextForScreen();
+  } else {
+    return Center(
+      child: AnimatedFigures(),
+    );
+  }
+}
